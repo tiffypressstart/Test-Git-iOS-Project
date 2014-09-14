12ad5c79f0f240de6826e0793f5cd2c0973e2ea2
Tiffy <Tiffy@Tiffys-Mac-Pro.local>
2014-09-13 20:06:14 -0700

    added a Test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Tiffy on 2014-09-13.
+//  Copyright (c) 2014 Tiffy Press Start. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Tiffy on 2014-09-13.
+//  Copyright (c) 2014 Tiffy Press Start. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
